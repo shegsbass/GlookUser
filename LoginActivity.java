32850506f87394a976bd8b48541b795d3ebcaaa3
shegsbass <oladokunoluwasegun@gmail.com>
2019-03-25 14:01:45 +0100

    Complete Login and signp authentication with Firebase

---
+++ LoginActivity.java
@@ -0,0 +1,97 @@
+package com.oladokun.shegs.glookuser;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+    public EditText EmailField, PasswordField;
+    TextView btnLogIn, btnSignup;
+    FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        firebaseAuth = FirebaseAuth.getInstance();
+        EmailField = (EditText) findViewById(R.id.emailID);
+        PasswordField = (EditText) findViewById(R.id.passwordID);
+        btnLogIn = (TextView) findViewById(R.id.login);
+        btnSignup = (TextView) findViewById(R.id.signup_Link);
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Toast.makeText(LoginActivity.this, "User logged in ", Toast.LENGTH_SHORT).show();
+                    Intent I = new Intent(LoginActivity.this, VendorHomeActivity.class);
+                    startActivity(I);
+                } else {
+                    Toast.makeText(LoginActivity.this, "Login to continue", Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent I = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(I);
+            }
+        });
+        btnLogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String userEmail = EmailField.getText().toString();
+                String userPaswd = PasswordField.getText().toString();
+                if (userEmail.isEmpty()) {
+                    EmailField.setError("Provide your Email first!");
+                    EmailField.requestFocus();
+                } else if (userPaswd.isEmpty()) {
+                    PasswordField.setError("Enter Password!");
+                    PasswordField.requestFocus();
+                } else if (userEmail.isEmpty() && userPaswd.isEmpty()) {
+                    Toast.makeText(LoginActivity.this, "Fields Empty!", Toast.LENGTH_SHORT).show();
+                } else if (!(userEmail.isEmpty() && userPaswd.isEmpty())) {
+
+                    progressBar.setVisibility(View.VISIBLE);
+                    firebaseAuth.signInWithEmailAndPassword(userEmail, userPaswd).addOnCompleteListener(LoginActivity.this, new OnCompleteListener() {
+                        @Override
+                        public void onComplete(@NonNull Task task) {
+                            progressBar.setVisibility(View.GONE);
+                            if (!task.isSuccessful()) {
+                                Toast.makeText(LoginActivity.this, "Not sucessfull", Toast.LENGTH_SHORT).show();
+                            } else {
+                                startActivity(new Intent(LoginActivity.this, VendorHomeActivity.class));
+                            }
+                        }
+                    });
+                } else {
+                    Toast.makeText(LoginActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(authStateListener);
+    }
+}
--- VendorHomeActivity.java
+++ VendorHomeActivity.java
@@ -59,13 +59,23 @@ public class VendorHomeActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_home) {
             // Handle the camera action
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_earnings) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_appointments) {
+
+        } else if (id == R.id.nav_payment) {
+
+        } else if (id == R.id.nav_profile) {
+
+        } else if (id == R.id.nav_services) {
+
+        } else if (id == R.id.nav_support) {
 
         } else if (id == R.id.nav_send) {
+
+        } else if (id == R.id.nav_share) {
 
         }
 
